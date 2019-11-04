504267b9c84c8386ad8c816ccf26de858d09ef27
Damo-99 <56005241+Damo-99@users.noreply.github.com>
2019-11-04 20:42:52 -0300

    Update todo_db-dml.sql

--- script/todo_db-dml.sql
+++ script/todo_db-dml.sql
@@ -3,3 +3,4 @@ INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NUL
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno3', 'aloumno', 'otro3', 'alumno3@gmail.com'); 
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno4', 'aloumno', 'otro4', 'alumno4@gmail.com'); 
 INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno5', 'aloumno', 'otro5', 'alumno5@gmail.com'); 
+INSERT INTO `tarea` (`id`, `descripcion`, `fecha_limite`, `realizada`, `fecha_realizada`, `id_persona`) VALUES (NULL, 'Crear repo en github', '2019-11-11', '', '0', '1') 
