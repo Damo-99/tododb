5c7feef7da06e0baadddd11181a679aeb05b917d
Damo-99 <56005241+Damo-99@users.noreply.github.com>
2019-11-04 20:37:20 -0300

    Create todo_db-dml.sql

---
+++ script/todo_db-dml.sql
@@ -0,0 +1,5 @@
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno1', 'aloumno', 'otro', 'alumno1@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno2', 'aloumno', 'otro2', 'alumno2@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno3', 'aloumno', 'otro3', 'alumno3@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno4', 'aloumno', 'otro4', 'alumno4@gmail.com'); 
+INSERT INTO `persona` (`id`, `alias`, `nombre`, `apellido`, `email`) VALUES (NULL, 'alumno5', 'aloumno', 'otro5', 'alumno5@gmail.com'); 
